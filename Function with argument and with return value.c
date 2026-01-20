08c09fe4db67a70833aa6fbd5bf54ae141be6896
Mohammed Saad M N <ms7710420@gmail.com>
2026-01-21 00:46:32 +0530

    Add files via upload

---
+++ Function with argument and with return value.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : Function.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int,int);
+int main(void) {
+		setbuf(stdout,NULL);
+		int a,b,result;
+		printf("Enter 2 numbers");
+		scanf("%d%d",&a,&b);
+		result=sum(a,b);
+		printf("Result is %d",result);
+
+
+	return EXIT_SUCCESS;
+}
+int sum(int num1,int num2){
+	int result;
+	result=num1+num2;
+	return result;
+}
