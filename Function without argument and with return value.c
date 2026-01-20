cf8a42ea87b3361d9544565649caff2a13a0cf57
Mohammed Saad M N <ms7710420@gmail.com>
2026-01-21 00:50:26 +0530

    Add files via upload

---
+++ Function without argument and with return value.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : Function.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int sum();
+int main() {
+		int k;
+		setbuf(stdout,NULL);
+		k=sum();
+		printf("sum=%d",k);
+		return 0;
+}
+	int sum(){
+		int a,b,c;
+		printf("Enter 2 numbers");
+		scanf("%d%d",&a,&b);
+		c=a+b;
+		return c;
+	}
